6088cc7004af90bf8fd44d124e61905352ad641f
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-07-31 16:54:13 +0600

    Navber and Home Update

--- project/src/components/Home.jsx
+++ project/src/components/Home.jsx
@@ -3,22 +3,22 @@ import Typed from 'react-typed';
 
 const Home = () => {
   return (
-    <div className='container text-white flex  sm:text-[10px]'>
-            <div className='max-w[800px] mt[-96px] w-full h-screen mx-auto text-center flex flex-col  
-           pt-[10%] sm:text-[10px] justify-left'>
-                    <h1 className='md:text-6xl sm:text-4xl md:text-6xl font bold md:py-6'>FARZANA YESMIN</h1>
-                    <p className='text-[#00df9a] md:text-4xl  text-2xl font-bold p-1 '>Software Engineer</p>
-                    <div className='md:flex justify-center items-center md:text-4xl pt-2'>
-                        <p className='sm:text-[15px]'>Also I have knwoledge about  </p>
-                        <Typed className='pl-2 sm:text-[15px] text-[#0e9f44ec]'strings={['Content Create', 'Graphics Design', 'Video Edit']} typeSpeed={100} backSpeed={60}  loop/>
+    <div className='container text-white flex  sm:text-[10px] flex-row lg:pl-[5%]'>
+            <div className='max-w[800px] mt[-96px] w-full h-1/2 mx-auto text-center flex flex-col  
+           lg:pt-[15%] md:pt-[18%] sm:text-[10px] lg:py-[20%]'>
+                    <h1 className='sm:text-2xl md:text-4xl text-1xl font bold '>FARZANA YESMIN</h1>
+                    <p className='text-[#00df9a] md:text-2xl sm:text-[15px] text-[10px] font-bold p-1 '>Software Engineer</p>
+                    <div className='md:flex justify-center items-center md:text-4xl text-1xl pt-2'>
+                        <p className='sm:text-[15px] text-[12px]'>Also I have knwoledge about  </p>
+                        <Typed className='pl-2 sm:text-[12px] text-[12px] text-[#0e9f44ec]'strings={['Content Create', 'Graphics Design', 'Video Edit']} typeSpeed={100} backSpeed={60}  loop/>
                     </div>
                     <div className='flex justify-around '>
-                    <button class='bg-[#00df9a] md:w-[200px] sm:w-[150px] rounded-md font-medium my-6 mx-auto px-0 md:py-3 sm:py-2 text-black md:text-2xl sm:text-[15px]'>Hire Me</button>
+                    <button class='bg-[#00df9a] md:w-[200px] sm:w-[200px] rounded-md font-medium my-6 md:mx-auto px-3 md:py-3 sm:py-3 text-black md:text-2xl sm:text-[8px] text-[15px]'>Hire Me</button>
                     </div>
                     
 
             </div>
-            <div class='lg:w-[750px] w-full h-1/2 '>
+            <div class='md:w-[1200px] w-full md:h-1/2 lg:h-1/4 lg:pr-[8%]  sm:h-1/2 lg:h-1/4 sm:pr-0'>
                 <img src='../assets/jule.png'/>
             </div>
 
--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {AiOutlineClose} from 'react-icons/ai';
 
 
 const Navbar = () => {
-    const[nav,setnav] = useState(false)
+    const[nav,setnav] = useState(true)
     
     const handleNav = () => {
       setnav(!nav) 
@@ -15,8 +15,8 @@ const Navbar = () => {
         <h1 className='w-full text-3xl font-bold text-[rgb(0,223,154)]'>PORTFOLIO</h1> 
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
-            <li className='p-4'>Education</li>
             <li className='p-4'>Skills</li>
+            <li className='p-4'>Education</li>
             <li className='p-4'>Contact</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
@@ -26,8 +26,8 @@ const Navbar = () => {
         <h1 className='w-full text-3xl font-bold text-[#00df9a] px-4 py-8'>PORTFOLIO</h1> 
         <ul className='p-4 uppercase'>
             <li className='p-4 border-b border-gray-600'>Home</li>
-            <li className='p-4 border-b border-gray-600'>Education</li>
             <li className='p-4 border-b border-gray-600'>Skills</li>
+            <li className='p-4 border-b border-gray-600'>Education</li>
             <li className='p-4'>Contact</li>
         </ul>
         </div>
